fbcf9694a7e7e35be928c7febda283df76665b09
HT_YangRongJi <2877233732@qq.com>
2023-12-03 01:41:23 +0800

    航天-杨荣基

--- Assets/Resources/scripts/SceneManager/InitializeScene/InitializeSceneContro.cs
+++ Assets/Resources/scripts/SceneManager/InitializeScene/InitializeSceneContro.cs
@@ -7,9 +7,7 @@ using UnityEngine.UI;
 
 public class InitializeSceneContro : MonoBehaviour
 {
-    [SerializeField] private Image backGround;
     [SerializeField] private TMP_Text loadingNumber;
-    [SerializeField] private Transform Animater;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,15 +21,11 @@ public class InitializeSceneContro : MonoBehaviour
     IEnumerator CheckInitalOver()
     {
         float num = 0;
-        float num2 = 0;
         string st = "";
         while (num<=100)
         {
             num += Time.deltaTime * 50;
-            num2+= Time.deltaTime * 200;
-            num2 %= 360;
             st = (int)num+"%";
-            Animater.rotation = Quaternion.Euler(new Vector3(0,0,num2));
             loadingNumber.text = st;
             yield return null;
         }
